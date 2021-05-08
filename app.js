c3471d02176d86e3fe58cbe50848de7f35817cee
miles <a09112332@gmail.com>
2021-05-08 18:03:23 +0800

    data

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ app.get('/hot/clothes', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   fs.readFile('./data/data.json', (err, data) => {
     res.send(data);
-  })
-})
+  });
+});
 
-//login adduser
+//register
 app.post('/register', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   const body = req.body;
@@ -40,11 +40,63 @@ app.post('/register', (req, res) => {
     fs.writeFile('./data/login.json', data, (err) => {
       if (err) return console.log('err');
       //status為1代表註冊成功
-      res.send('1')
-    })
-  })
+      res.send('1');
+    });
+  });
+});
 
-})
+//login
+app.post('/login', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const body = req.body;
+  fs.readFile('./data/login.json', (err, data) => {
+    let userData = JSON.parse(data);
+    const flag = userData[0].user.some(item => {
+      return item.registerAccount == body.account && item.registerPsw == body.psw;
+    })
+    if (flag) {
+      const user = userData[0].user.find(item => {
+        if (item.registerAccount == body.account) return item;
+      })
+      const wish = user.wishList;
+      //status為1代表登入成功
+      res.send({ status: "1", wish });
+    } else {
+      //status為0代表登入失敗(並無此用戶)
+      res.send('0');
+    }
+  })
+});
+
+//wish_list
+app.get('/wish', (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  const wishList = req.query.data.wishList;
+  const account = req.query.data.account;
+  if (wishList !== undefined) {
+    fs.readFile('./data/login.json', 'utf8', (err, res) => {
+      let data = JSON.parse(res);
+      const index = data[0].user.findIndex(item => item.registerAccount === account)
+      data[0].user[index].wishList = wishList
+      data = JSON.stringify(data);
+      fs.writeFile('./data/login.json', data, (err) => {
+        if (err) console.log(err);
+      });
+    });
+  } else {
+    fs.readFile('./data/login.json', 'utf8', (err, res) => {
+      let data = JSON.parse(res);
+      const index = data[0].user.findIndex(item => item.registerAccount === account)
+      data[0].user[index].wishList = [];
+      data = JSON.stringify(data);
+      console.log(data);
+      fs.writeFile('./data/login.json', data, (err) => {
+        if (err) console.log(err);
+      });
+    });
+  };
+  res.send('1');
+});
 
 app.listen(3000, () => {
   console.log('running')
